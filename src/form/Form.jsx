7833aafb926aac0c20948900dd2264cc9ce357c0
fabian <fabiandavid2403@gmail.com>
2022-10-15 12:52:43 -0500

    main: form up

---
+++ src/form/Form.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+export default function Form() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ user, password });
+  };
+
+  const handleChangeUser = (e) => {
+    setUser(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        User:
+        <input type="text" onChange={handleChangeUser} />
+      </label>
+      <label>
+        Password:
+        <input type="text" onChange={handleChangePassword} />
+      </label>
+      <input type="submit" value="Enter" />
+    </form>
+  );
+}
